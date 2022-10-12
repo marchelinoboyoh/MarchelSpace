13e65b4d662fec689dc19fcf633669aab1101d9e
marchelinoboyoh <s21710442@student.unklab.ac.id>
2022-10-12 08:39:29 +0800

    second

--- js/type.js
+++ js/type.js
@@ -2,7 +2,7 @@ const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
 
-const textArray = ["Marchel", "UI/UX Designer", "Project Manager"];
+const textArray = ["Marchel", "UI/UX Designer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000;
